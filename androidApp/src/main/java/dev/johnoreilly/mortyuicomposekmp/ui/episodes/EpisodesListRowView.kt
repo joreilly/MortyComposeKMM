b198b9f9db833a871cf15604feceb168bb35a77c
John O'Reilly <oreillyj@gmail.com>
2020-12-22 14:20:05 +0000

    fix episode row formatting

--- androidApp/src/main/java/dev/johnoreilly/mortyuicomposekmp/ui/episodes/EpisodesListRowView.kt
+++ androidApp/src/main/java/dev/johnoreilly/mortyuicomposekmp/ui/episodes/EpisodesListRowView.kt
@@ -4,24 +4,29 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ProvideTextStyle
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import dev.johnoreilly.mortyuicomposekmp.GetEpisodesQuery
 
 @Composable
-fun EpisodesListRowView(episode: GetEpisodesQuery.Result) {
+fun EpisodesListRowView2(episode: GetEpisodesQuery.Result) {
 
     Row(modifier = Modifier.fillMaxWidth().clickable(onClick = {  }).padding(16.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
 
-        Column(modifier = Modifier.padding(start = 8.dp, end = 8.dp)) {
+        Column(modifier = Modifier.padding(start = 8.dp, end = 8.dp).weight(1f)) {
             Text(episode.name ?: "", style = MaterialTheme.typography.h6)
             Text(episode.episode ?: "",
+                maxLines = 1, overflow = TextOverflow.Ellipsis,
                 style = MaterialTheme.typography.subtitle2)
         }
 
@@ -34,4 +39,23 @@ fun EpisodesListRowView(episode: GetEpisodesQuery.Result) {
 
     }
     Divider()
+}
+
+@Composable
+fun EpisodesListRowView(episode: GetEpisodesQuery.Result) {
+    Row(verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp).fillMaxWidth()) {
+
+        Column(modifier = Modifier.weight(1f)) {
+            Text(episode.name ?: "",
+                style = MaterialTheme.typography.h6,
+                maxLines = 1, overflow = TextOverflow.Ellipsis,)
+            Text(episode.episode ?: "",
+                style = MaterialTheme.typography.subtitle2)
+
+        }
+
+
+        Text(episode.air_date ?: "", modifier = Modifier.padding(start = 16.dp))
+    }
 }
