9602c2a47d90fef21a5b0e3c6fc1f00e278bc1bd
John O'Reilly <oreillyj@gmail.com>
2020-12-22 14:28:26 +0000

    fix episode row formatting

--- androidApp/src/main/java/dev/johnoreilly/mortyuicomposekmp/ui/episodes/EpisodesListRowView.kt
+++ androidApp/src/main/java/dev/johnoreilly/mortyuicomposekmp/ui/episodes/EpisodesListRowView.kt
@@ -1,45 +1,15 @@
 package dev.johnoreilly.mortyuicomposekmp.ui.episodes
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.ProvideTextStyle
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import dev.johnoreilly.mortyuicomposekmp.GetEpisodesQuery
 
-@Composable
-fun EpisodesListRowView2(episode: GetEpisodesQuery.Result) {
-
-    Row(modifier = Modifier.fillMaxWidth().clickable(onClick = {  }).padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-
-        Column(modifier = Modifier.padding(start = 8.dp, end = 8.dp).weight(1f)) {
-            Text(episode.name ?: "", style = MaterialTheme.typography.h6)
-            Text(episode.episode ?: "",
-                maxLines = 1, overflow = TextOverflow.Ellipsis,
-                style = MaterialTheme.typography.subtitle2)
-        }
-
-        Column(horizontalAlignment = Alignment.End,
-            modifier = Modifier.fillMaxWidth().padding(end = 16.dp))
-        {
-            Text(episode.air_date ?: "",
-                style = MaterialTheme.typography.subtitle2, color = Color.Gray)
-        }
-
-    }
-    Divider()
-}
 
 @Composable
 fun EpisodesListRowView(episode: GetEpisodesQuery.Result) {
@@ -52,9 +22,7 @@ fun EpisodesListRowView(episode: GetEpisodesQuery.Result) {
                 maxLines = 1, overflow = TextOverflow.Ellipsis,)
             Text(episode.episode ?: "",
                 style = MaterialTheme.typography.subtitle2)
-
         }
-
 
         Text(episode.air_date ?: "", modifier = Modifier.padding(start = 16.dp))
     }
