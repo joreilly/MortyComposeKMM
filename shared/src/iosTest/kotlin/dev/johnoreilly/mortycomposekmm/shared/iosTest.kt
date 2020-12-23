a56121db8106f2b4f146d6779243ae2682f5ef08
John O'Reilly <oreillyj@gmail.com>
2020-12-23 14:11:22 +0000

    test update

--- shared/src/iosTest/kotlin/dev/johnoreilly/mortycomposekmm/shared/iosTest.kt
+++ shared/src/iosTest/kotlin/dev/johnoreilly/mortycomposekmm/shared/iosTest.kt
@@ -1,45 +1,18 @@
 package dev.johnoreilly.mortycomposekmm.shared
 
-import com.apollographql.apollo.ApolloClient
-import com.apollographql.apollo.api.ApolloExperimental
-import com.apollographql.apollo.api.Input
-import com.apollographql.apollo.network.http.ApolloHttpNetworkTransport
-import dev.johnoreilly.mortycomposekmm.GetCharactersQuery
-import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.runBlocking
 import kotlin.test.Test
 
 class IosGreetingTest {
 
-    @InternalCoroutinesApi
-    @ApolloExperimental
     @Test
     fun testExample() {
+        // TODO this doesn't get passed getCharacters call right now....need to figure out why!
         runBlocking {
+            val repository = MortyRepository()
 
-            val apolloClient = ApolloClient(
-                networkTransport = ApolloHttpNetworkTransport(
-                    serverUrl = "https://rickandmortyapi.com/graphql",
-                    headers = mapOf(
-                        "Accept" to "application/json",
-                        "Content-Type" to "application/json",
-                    )
-                )
-            )
-
-            println("BEFORE")
-            // execute() returns a Flow, here we only take the first item
-            val response = apolloClient.query(GetCharactersQuery(page = Input.optional(0))).execute()
-
-            println("after, response = $response")
-
-            response.collect {
-                println(it.data)
-            }
-
-
+            val charactersResponse = repository.getCharacters(0)
+            println(charactersResponse.data?.characters)
         }
-
     }
 }
