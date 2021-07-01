71f89d4f99f24bba05fa99c694d6a1c5bb0119d4
John O'Reilly <oreillyj@gmail.com>
2021-07-01 18:50:14 +0100

    activity-compose 1.3.0-rc01

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -59,7 +59,7 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-compose:1.3.0-beta02")
+    implementation("androidx.activity:activity-compose:1.3.0-rc01")
     implementation("com.google.android.material:material:1.3.0")
 
     implementation(Compose.ui)
