839d4f9cb5f223013aa514c6030570100f5d611d
John O'Reilly <oreillyj@gmail.com>
2021-07-14 21:42:13 +0100

    Jetpack Compose rc2 + related dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -59,8 +59,8 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-compose:1.3.0-rc01")
-    implementation("com.google.android.material:material:1.3.0")
+    implementation("androidx.activity:activity-compose:1.3.0-rc02")
+    implementation("com.google.android.material:material:1.4.0")
 
     implementation(Compose.ui)
     implementation(Compose.runtime)
--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -5,10 +5,10 @@ object Versions {
     const val apollo = "3.0.0-alpha01"
     const val multiplatformPaging = "0.4.1"
 
-    const val compose = "1.0.0-rc01"
+    const val compose = "1.0.0-rc02"
     const val nav_compose = "2.4.0-alpha04"
     const val paging_compose = "1.0.0-alpha11"
-    const val accompanist = "0.13.0"
+    const val accompanist = "0.14.0"
 
     const val junit = "4.13"
     const val testRunner = "1.3.0"
